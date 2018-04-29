2cc69075273e64cb370e86cd57c7ecc418757426
Eric C. Singer <ericcsinger@gmail.com>
2018-04-29 10:35:18 -0400

    Migrated Powershell Modules to dedicated repo
    I moved my Powershell modules to thier own dedicated modules

--- Modules/ECS.LocalGPO/1.0.0/ECS.LocalGPO.psm1
+++
@@ -1,5 +0,0 @@
-﻿#Loading Local GPO Functions
-
-. "$PSScriptRoot\Functions\Get-ECSLocalGPOUserRightAssignment.ps1"
-. "$PSScriptRoot\Functions\Add-ECSLocalGPOUserRightAssignment.ps1"
-. "$PSScriptRoot\Functions\Remove-ECSLocalGPOUserRightAssignment.ps1"
